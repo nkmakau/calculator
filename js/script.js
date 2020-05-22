ff4448e649022f68bdd22b949825205abcee0209
Noah Makau <noshmak@gmail.com>
2020-05-21 20:46:11 -0700

    add substraction, multiplacation, division functions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ var add = function(num1, num2) {
     return num1 + num2;
   }
 
+Var subtract =function(num1, num2) {
+  return num1 - num2
+}
+
+var multiply = function(num1, num2) {
+  return num1 * num2
+}
+var divide = function(num1, num2) {
+  return num1 / num2
+}
 
   var num1 = parseInt(prompt("Enter a kligong number"));
   var num2 = parseInt(prompt("Enter another kligong number"));
