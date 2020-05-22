07d8d5f5d6f39046a4127f6f28a19430d81aa007
Noah Makau <noshmak@gmail.com>
2020-05-21 20:38:24 -0700

    add more var and tidy js code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var add = function(num1, num2) {
     return num1 + num2;
   }
+
+  
   var num1 = parseInt(prompt("Enter a kligong number"));
   var num2 = parseInt(prompt("Enter another kligong number"));
-  alert(add(num1, num2));
+  var answer = add(num1, num2)
+  alert(result)
