7b47a490e5b2e84648ad947dbd14303577d2ac78
Noah Makau <noshmak@gmail.com>
2020-05-21 20:40:47 -0700

    correct alert code

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ var add = function(num1, num2) {
     return num1 + num2;
   }
 
-  
+
   var num1 = parseInt(prompt("Enter a kligong number"));
   var num2 = parseInt(prompt("Enter another kligong number"));
   var answer = add(num1, num2)
-  alert(result)
+  alert(answer)
