cbc5e02d95d89c204b87b4bf4e44a67e6448036b
Noah Makau <noshmak@gmail.com>
2020-05-21 20:25:41 -0700

    added calculator files for js practice

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var add = function(num1, num2) {
+    return num1 + num2;
+  }
+  var num1 = parseInt(prompt("Enter a kligong number"));
+  var num2 = parseInt(prompt("Enter another kligong number"));
+  alert(add(num1, num2));
