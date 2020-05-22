ea6d43101a208999f8deded355ce6000dcdda361
Noah Makau <noshmak@gmail.com>
2020-05-21 20:50:05 -0700

    correct capitalization error on var in script.js:5

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var add = function(num1, num2) {
     return num1 + num2;
   }
 
-Var subtract =function(num1, num2) {
+var subtract = function(num1, num2) {
   return num1 - num2
 }
 
@@ -15,5 +15,5 @@ var divide = function(num1, num2) {
 
   var num1 = parseInt(prompt("Enter a kligong number"));
   var num2 = parseInt(prompt("Enter another kligong number"));
-  var answer = add(num1, num2)
+  var answer = subtract(num1, num2)
   alert(answer)
