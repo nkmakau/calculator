ce106e1ddcd9cbf7004d5322d91535ffab76c5a3
Noah Makau <noshmak@gmail.com>
2020-06-10 15:18:16 +0300

    added UI and a nice form

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
-var add = function(num1, num2) {
+//Business (back-end) logic:
+var add = function (num1, num2) {
   return num1 + num2;
 }
 
-var subtract = function(num1, num2) {
+var subtract = function (num1, num2) {
   return num1 - num2
 }
 
-var multiply = function(num1, num2) {
+var multiply = function (num1, num2) {
   return num1 * num2
 }
-var divide = function(num1, num2) {
+var divide = function (num1, num2) {
   return num1 / num2
 }
 
-var num1 = parseInt(prompt("Enter a kligong number"));
-var num2 = parseInt(prompt("Enter another kligong number"));
-var answer = divide(num1, num2)
-alert(answer)
+//Everything below this line is UI (Front-end) logic:
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault()
+    var num1 = parseInt($("#add1").val())
+    var num2 = parseInt($("#add2").val())
+    var answer = add(num1, num2)
+    $("#output").text(answer)
+  })
+})
